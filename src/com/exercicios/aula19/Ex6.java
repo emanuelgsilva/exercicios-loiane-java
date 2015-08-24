7fe8b5b50c8c95a7b2601a3a4a4878cf7ce21d7a
maugravena <maugravena@gmail.com>
2015-08-24 16:50:14 -0300

    Initial commit

---
+++ src/com/exercicios/aula19/Ex6.java
@@ -0,0 +1,50 @@
+package com.exercicios.aula19;
+
+import java.util.Scanner;
+
+/**
+ * Created by mau on 24/08/15.
+ */
+
+/*
+Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
+um vetor C, onde cada elemento de C é a soma dos respectivos
+elementos em A e B, ou seja:
+C[i] = A[i] + B[i]
+ */
+public class Ex6 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+        int[] vetorC = new int[vetorA.length];
+
+        int i;
+
+        System.out.println("Vetor A");
+
+        for (i = 0; i < vetorA.length; i++) {
+            System.out.println("Digite o valor da posição " + i + ": ");
+            vetorA[i] = scan.nextInt();
+        }
+
+        System.out.println("Vetor B");
+
+        for (i = 0; i < vetorB.length; i++) {
+            System.out.println("Digite o valor da posição " + i + ": ");
+            vetorB[i] = scan.nextInt();
+
+            vetorC[i] = vetorA[i] + vetorB[i];
+        }
+
+        System.out.println("Vetor C");
+
+        for (i = 0; i < vetorC.length; i++) {
+
+            System.out.print(vetorC[i] + ", ");
+        }
+    }
+}
